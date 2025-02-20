11baa14c34ecb36d0fd99a3a39601f04f2990d8c
Stefnotch <brandmairstefan@gmail.com>
2025-02-20 16:44:56 +0100

    Use doc comments for test schema

--- src/TestSchema.ts
+++ src/TestSchema.ts
@@ -1,10 +1,15 @@
-// prettier-ignore
 export interface WgslTestSrc {
-  name: string;                     // human readable description of test
-  weslSrc: Record<string, string>;  // source wesl+ texts, keys are file paths
-  notes?: string;                   // additional notes to test implementors
-  expectedWgsl?: string;            // expected linked result wgsl 
-  underscoreWgsl?: string;          // expected linked result wgsl using underscore mangler
+  /** human readable description of test */
+  name: string;
+  /** source wesl+ texts, keys are file paths */
+  weslSrc: Record<string, string>;
+  /** additional notes to test implementors */
+  notes?: string;
+  /**
+   * Expected linked result wgsl.
+   * Uses the length prefixing mangling strategy, see [NameMangling.md](https://github.com/wgsl-tooling-wg/wesl-spec/blob/main/NameMangling.md)
+   */
+  expectedWgsl?: string;
 }
 
 export interface ParsingTest {
@@ -12,11 +17,15 @@ export interface ParsingTest {
   fails?: true;
 }
 
-// prettier-ignore
 export interface BulkTest {
-  name: string;           // human readable name of test set
-  baseDir: string;        // directory within https://github.com/wgsl-tooling-wg/community-wgsl 
-  exclude?: string[];     // exclude files containing these strings or regexes
-  include?: string[];     // names of test files inside of baseDir ('/' as separator for partial paths)
-  globInclude?: string[]; // glob patters of test files
+  /** human readable name of test set */
+  name: string;
+  /** directory within https://github.com/wgsl-tooling-wg/community-wgsl  */
+  baseDir: string;
+  /** exclude files containing these strings or regexes */
+  exclude?: string[];
+  /** names of test files inside of baseDir ('/' as separator for partial paths) */
+  include?: string[];
+  /** glob patters of test files */
+  globInclude?: string[];
 }
